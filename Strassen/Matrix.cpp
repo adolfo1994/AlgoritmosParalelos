f4602ccb10b34f553389d9c18a9cdc1aecf97bfe
adolfo1994 <adolfo@inka-labs.com>
2015-04-01 00:36:05 -0500

    Strassen multiplication

--- Strassen/Matrix.cpp
+++ Strassen/Matrix.cpp
@@ -2,118 +2,156 @@
 #include <random>
 #include <pthread.h>
 #include <algorithm>
-#include "utils.cpp"
 
 using namespace std;
 
 class Matrix
 {
 public:
-	Matrix(int _f, int _c){
-		rows = _f;
-		cols = _c;
-		mat = new int * [rows];
-		for(int i=0; i < rows; i++){
-			mat[i] = new int[cols];
+	Matrix(int dim_, bool rand_, bool normalize){
+		dim = dim_;
+		if(normalize){
+			int n_dim = 2;
+			while(n_dim < dim_)
+				n_dim *= 2;
+			dim = n_dim;
 		}
-		random_device rd;
-    	mt19937 gen(rd());
-    	uniform_int_distribution<> dis(1, 10001);
-    	for (int i = 0; i < rows; ++i)
-    	{
-    		for (int j = 0; j < cols; ++j)
-    		{
-    			mat[i][j] = dis(gen);
-    		}
-    	}
+
+		mat = new int[dim * dim];
+		if(rand_){
+			random_device rd;
+    		mt19937 gen(rd());
+    		uniform_int_distribution<> dis(1, 10);
+    		for (int i = 0; i < dim*dim; ++i)
+    		{    		
+	    		mat[i] = dis(gen);    		
+    		}	
+		}
+	}	
+	inline int& operator()(int i, int j){
+		return mat[i*dim + j];
 	}
-	Matrix (const Matrix &other){
-		rows = other.rows;
-		cols = other.cols;
-		mat = new int * [rows];
-		for(int i=0; i < rows; i++){
-			mat[i] = new int[cols];
-		}
-		for (int i = 0; i < rows; ++i)
+	inline int operator()(int i, int j) const{
+		return mat[i*dim + j];
+	}
+	Matrix operator + (Matrix b){
+		Matrix c(dim, false, false);
+		for (int i = 0; i < dim; ++i)
 		{
-			for (int j = 0; j < cols; ++j)
+			for (int j = 0; j < dim; ++j)
 			{
-				mat[i][j] = other.mat[i][j];		
+				c(i,j) = (*this)(i, j) + b(i, j);
 			}
 		}
+		return c;
 	}
-	void print(){
-		for (int i = 0; i < rows; ++i)
+	Matrix operator - (Matrix b){
+		Matrix c(dim, false, false);
+		for (int i = 0; i < dim; ++i)
 		{
-			for (int j = 0; j < cols; ++j)
+			for (int j = 0; j < dim; ++j)
 			{
-				cout << mat[i][j] << "\t";
+				c(i,j) = (*this)(i, j) - b(i, j);
+			}
+		}
+		return c;
+	}
+
+	void print(){
+		for (int i = 0; i < dim; ++i)
+		{
+			for (int j = 0; j < dim; ++j)
+			{
+				cout << (*this)(i, j) << "\t";
 			}
 			cout<<endl;
 		}
-	}
-	void normalize(int final){
-		int ** n_mat = new int* [final];
-		for (int i = 0; i < final; ++i)
-		{
-			n_mat[i] = new int [final];
-		}
-		for (int i = 0; i < final; ++i)
-		{
-			for (int j = 0; j < final; ++j)
-			{
-				if(i < rows && j < cols){
-					n_mat[i][j] = mat[i][j];
-				}else{
-					n_mat[i][j] = 0;
-				}
-			}
-		}
-		mat = n_mat;
-		rows = final;
-		cols = final;
+	}	
 
+	~Matrix(){		
 	}
 
-	~Matrix(){
-		for (int i = 0; i < rows; ++i)
-		{
-			delete mat[i];
-		}
-		delete mat;
-	}
-
-	int rows;
-	int cols;
-	int ** mat;	
+	int dim;	
+	int *mat;	
 };
 
-Matrix strassen(Matrix a, Matrix b){
-	int rows = max(a.rows, b.rows);
-	int cols = max(a.cols, b.cols);
-	int max_cr = max(rows, cols);
-	if(isPowerOfTwo(max_cr)){
-		a.normalize(max_cr);
-		b.normalize(max_cr);
-	}	
-	else{
-		while(!isPowerOfTwo(max_cr)){
-			max_cr++;
+Matrix reg_mult(Matrix a, Matrix b) {
+        Matrix c(a.dim, false, false);
+        for (int i = 0; i < a.dim; i++){
+            for (int j = 0; j < a.dim; j++){
+            	c(i,j) = 0;
+            	for (int k = 0; k < a.dim; k++)
+                    c(i,j) += a(i,k) * b(k,j);       
+            }
+        }
+        return c;
+}
+
+Matrix parte(int pi, int pj, Matrix m){
+	Matrix p(m.dim/2 , false, true);
+	pi = pi * p.dim;
+	pj = pj * p.dim;
+	for (int i = 0; i < p.dim; ++i)
+	{
+		for (int j = 0; j < p.dim; ++j)
+		{
+			p(i,j) = m(i + pi, j + pj);
 		}
-		a.normalize(max_cr);
-		b.normalize(max_cr);
+	}	
+	return p;
+}
+
+void unir(int pi, int pj, Matrix* m, Matrix p){
+	pi = pi * p.dim;
+	pj = pj * p.dim;
+	for (int i = 0; i < p.dim; ++i)
+	{
+		for (int j = 0; j < p.dim; ++j)
+		{
+			(*m)(i + pi, j + pj) = p(i, j);
+		}
+	}	
+}
+
+Matrix strassen(Matrix a, Matrix b){
+	if (a.dim < 2){
+		return reg_mult(a, b);
 	}
-	a.print();
-	cout<<endl;
-	b.print();
-	cout<< "no pasa nada"<<endl;
-	return a;
+	Matrix a11 = parte(0, 0, a);
+	Matrix a12 = parte(0, 1, a);
+	Matrix a21 = parte(1, 0, a);
+	Matrix a22 = parte(1, 1, a);
+
+	Matrix b11 = parte(0, 0, b);
+	Matrix b12 = parte(0, 1, b);
+	Matrix b21 = parte(1, 0, b);
+	Matrix b22 = parte(1, 1, b);
+
+	Matrix m1 = strassen(a11+a22, b11+b22);
+	Matrix m2 = strassen(a21+a22, b11);
+	Matrix m3 = strassen(a11, b12-b22);
+	Matrix m4 = strassen(a22, b21-b11);
+	Matrix m5 = strassen(a11+a22, b22);
+	Matrix m6 = strassen(a21-a11, b11+b12);
+	Matrix m7 = strassen(a12-a22, b21+b22);
+
+	Matrix ans(a.dim, false, true);
+	unir(0, 0, &ans, m1+m4 - m5+m7);
+	unir(0, 1, &ans, m3+m5);
+	unir(1, 0, &ans, m2+m4);
+	unir(1, 1, &ans, m1-m2+m3+m6);
+	return ans;
 }
 
 int main(int argc, char const *argv[])
 {	
-	Matrix * a = new Matrix(14,14);
-	Matrix * b = new Matrix(14,12);
-	Matrix c = strassen(*a, *b);
+	Matrix a(2023, true, false);
+	a.print();
+    cout<<endl;
+    Matrix b(2023, true, false);
+    b.print();
+    cout<<endl;
+    Matrix c = strassen(a, b);
+    c.print();
 	return 0;
 }
